65dc04af575bc1c6ae3f68c14e56cdbff898e52a
ErickSchuenck <esfmokdeci@gmail.com>
2022-05-07 15:30:25 -0300

    criadas funções para entrar no app

--- src/LoginScreen.js
+++ src/LoginScreen.js
@@ -1,20 +1,75 @@
 import React from 'react'
-import { Link } from "react-router-dom";
+import axios from 'axios'
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from 'react';
+
 
 export default function LoginScreen() {
+
+  const URL = ''
+  const navigate = useNavigate()
+
+  const [login, setLogin] = useState(
+    {
+      email: '',
+      password: '',
+    }
+  )
+
+  const [data, setData] = useState(
+    {
+      email: '',
+      name: '',
+      transactions: '',
+      token: '',
+    }
+  )
+
+  function enterApp() {
+    // setLoading(true)
+    axios.post(URL, login)
+      .then(response => {
+        console.log('response', response)
+        setData(response.data);
+        // localStorage.setItem('login', JSON.stringify(response.data.token))
+        navigate('/main')
+      })
+
+      .catch(error => {
+        // setLoading(false)
+        console.log(error);
+        alert("Erro de login, favor checar novamente os dados. Se não possui uma conta, experimente se cadastrar antes!");
+      }
+      )
+  }
+
   return (
     <>
       <div className='logo'>
         <p>My Wallet</p>
       </div>
       <div className='user-input'>
-        <input type={'text'} placeholder={'E-mail'} />
+        <input
+          type={'text'}
+          placeholder={'E-mail'}
+          value={login.email}
+          onChange={(event) => {
+            setLogin({ login, email: event.target.value })
+          }}
+        />
       </div>
       <div className='user-input'>
-        <input type={'text'} placeholder={'Senha'} />
+        <input
+          type={'text'}
+          placeholder={'Senha'}
+          value={login.password}
+          onChange={(event) => {
+            setLogin({ login, password: event.target.value })
+          }}
+        />
       </div>
       <div className='confirm-button'>
-        <button>
+        <button onClick={() => enterApp()}>
           <p>Entrar</p>
         </button>
       </div>
