b27443eda3e8bbfb1cbee8a027becb7ba6d4ab58
ErickSchuenck <esfmokdeci@gmail.com>
2022-05-06 21:26:10 -0300

    aprimorando design da tela principal

--- src/MainScreen.js
+++ src/MainScreen.js
@@ -2,9 +2,23 @@ import React from 'react'
 
 export default function MainScreen() {
   return (
-    <div className='header'>
-      <h1>Olá, Fulano</h1>
-      <ion-icon name="exit-outline"></ion-icon>
-    </div>
+    <>
+      <div className='container'>
+        <div className='header'>
+          <h1>Olá, Fulano</h1>
+          <ion-icon name="exit-outline"></ion-icon>
+        </div>
+        <div className='account-container'>
+        </div>
+        <div className='report-transaction-button'>
+          <p>Nova <br />entrada</p>
+          <ion-icon name="add-circle-outline"></ion-icon>
+        </div>
+        <div className='report-transaction-button'>
+          <p>Nova <br />saida</p>
+          <ion-icon name="remove-circle-outline"></ion-icon>
+        </div>
+      </div>
+    </>
   )
 }
