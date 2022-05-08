7c7796ad32d4bb081a6c461b1e8d215980ffccc0
ErickSchuenck <esfmokdeci@gmail.com>
2022-05-08 18:57:15 -0300

    agora telas de entrada e saida enviam dados

--- src/EntradaScreen.js
+++ src/EntradaScreen.js
@@ -1,8 +1,30 @@
-import React, { useState } from 'react'
+import axios from 'axios'
+import React from 'react'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export default function EntradaScreen() {
 
+  const URL = 'http://localhost:5000/deposit'
+  const navigate = useNavigate()
 
+  const [entrada, setEntrada] = useState({
+    value: 0,
+    type: 'entrada',
+    title: '',
+  })
+
+  function send() {
+    axios.post(URL, entrada)
+      .then(response => {
+        console.log('response', response)
+        navigate('/main')
+      })
+      .catch(error => {
+        console.log(error)
+        alert(error)
+      })
+  }
 
   return (
     <>
@@ -11,13 +33,25 @@ export default function EntradaScreen() {
       </div>
       <div className='value-container'>
         <div className='value-input'>
-          <input type={'text'} placeholder={'Valor'} />
+          <input
+            type={'text'}
+            placeholder={'Valor'}
+            onChange={(event) => {
+              setEntrada({ ...entrada, value: event.target.value })
+            }}
+          />
         </div>
         <div className='value-input'>
-          <input type={'text'} placeholder={'Descrição'} />
+          <input
+            type={'text'}
+            placeholder={'Descrição'}
+            onChange={(event) => {
+              setEntrada({ ...entrada, title: event.target.value })
+            }}
+          />
         </div>
         <div className='value-confirm-button'>
-          <button>
+          <button onClick={() => send()}>
             <p>Salvar Entrada</p>
           </button>
         </div>
--- src/SaidaScreen.js
+++ src/SaidaScreen.js
@@ -1,13 +1,30 @@
+import axios from 'axios'
 import React from 'react'
 import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export default function SaidaScreen() {
+
+  const URL = 'http://localhost:5000/withdraw'
+  const navigate = useNavigate()
 
   const [saida, setSaida] = useState({
     value: 0,
     type: 'saida',
     title: '',
   })
+
+  function send() {
+    axios.post(URL, saida)
+      .then(response => {
+        console.log('response', response)
+        navigate('/main')
+      })
+      .catch(error => {
+        console.log(error)
+        alert(error)
+      })
+  }
 
   return (
     <>
@@ -34,7 +51,7 @@ export default function SaidaScreen() {
           />
         </div>
         <div className='value-confirm-button'>
-          <button onClick={() => console.log(saida)}>
+          <button onClick={() => send()}>
             <p>Salvar Saida</p>
           </button>
         </div>
